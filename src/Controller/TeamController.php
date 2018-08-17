7a491f480081025dab41b7216e21e2bc78a1f77d
kovosz <kovacs.oszkar.84@gmail.com>
2018-08-17 18:32:38 +0200

    - Enable sessions - Create new team success page - Refactor Team form to its own class

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -4,26 +4,24 @@
 namespace App\Controller;
 
 use App\Entity\Team;
+use App\Form\TeamType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-class AssessmentController extends Controller
+class TeamController extends Controller
 {
-
-    public function new(Request $request)
+    /**
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function new(Request $request, SessionInterface $session)
     {
         // just setup a fresh $team object (remove the dummy data)
         $team = new Team();
 
-        $form = $this->createFormBuilder($team)
-            ->add('name', TextType::class)
-            ->add('releaseTrain', TextType::class)
-            ->add('size', IntegerType::class)
-            ->add('save', SubmitType::class, array('label' => 'Create Team'))
-            ->getForm();
+        $form = $this->createForm(TeamType::class, $team);
 
         $form->handleRequest($request);
 
@@ -32,17 +30,31 @@ class AssessmentController extends Controller
             // but, the original `$team` variable has also been updated
             $team = $form->getData();
 
+            $session->set('team', $team);
             // ... perform some action, such as saving the team to the database
             // for example, if Task is a Doctrine entity, save it!
             // $entityManager = $this->getDoctrine()->getManager();
             // $entityManager->persist($team);
             // $entityManager->flush();
 
-            return $this->redirectToRoute('team_success');
+            return $this->redirectToRoute('app_team_success');
         }
 
         return $this->render('team/new.html.twig', array(
             'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function success(SessionInterface $session)
+    {
+        $team = $session->get('team');
+        return $this->render('team/success.html.twig', array(
+            'team' => $team,
         ));
     }
 }
---
+++ src/Form/TeamType.php
@@ -0,0 +1,24 @@
+<?php
+// src/Form/TeamType.php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class TeamType extends AbstractType
+{
+    /**
+     * @inheritdoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('releaseTrain', TextType::class)
+            ->add('size', IntegerType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Team'));
+    }
+}
